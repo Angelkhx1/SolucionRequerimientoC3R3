13c9ecc3623d4c78b086435b0f9f701ff70a7011
Angelkhx1 <53708125+Angelkhx1@users.noreply.github.com>
2022-10-17 13:44:00 -0500

    Add files via upload

---
+++ Misionticreto/src/main/java/com/misiontic/ciclo3/Misionticreto/MisionticretoApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.ciclo3.Misionticreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisionticretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisionticretoApplication.class, args);
+	}
+
+}
---
+++ Misionticreto/src/test/java/com/misiontic/ciclo3/Misionticreto/MisionticretoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.ciclo3.Misionticreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisionticretoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
